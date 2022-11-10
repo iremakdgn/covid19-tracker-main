45881d864d3d659f37ca870ed458041837a94961
iremakdgn <irem.akdgn.1996@gmail.com>
2022-11-10 15:14:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# COVID-19 TRACKER
+
+Display of infected,recovered, deaths and active cases by country
+
+- React
+- Redux
+- Axios
+- API
+- React-Bootstrap and Bootstrap
+- Fortawesome
+- React-moment
+- React-chartjs-2 ans chart.js
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cards from './components/cards';
+import Graphic from './components/graphic';
+import Header from './components/header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Cards />
+      <Graphic />
+    </div>
+  );
+}
+
+export default App;
